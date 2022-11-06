e46e13067519d27794783db5c7c128883faf84d2
25-Khairin <mohdkhairin62@gmail.com>
2022-11-07 02:26:45 +0800

    Create empty.ps1
    added script for irm

---
+++ PS_File/empty.ps1
@@ -0,0 +1,19 @@
+# Enable TLSv1.2 for compatibility with older clients
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
+
+$DownloadURL = 'https://raw.githubusercontent.com/8mpty/Windows_Post_Install_BATCH/main/Install.cmd'
+
+$FilePath = "$env:TEMP\Post_Install.cmd"
+
+try {
+    Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing -OutFile $FilePath
+} catch {
+    Write-Error $_
+	Return
+}
+
+if (Test-Path $FilePath) {
+    Start-Process $FilePath -Wait
+    $item = Get-Item -LiteralPath $FilePath
+    $item.Delete()
+}
