b4336cc95425ba8afc0c0e79e637352a03e88c0e
8mpty <mohdkhairin62@gmail.com>
2023-05-09 11:38:37 +0800

    revert stupid file name changes for domain testing as it is not related

--- psfile/empty.ps1
+++ psfile/empty.ps1
@@ -1,9 +1,9 @@
 # Enable TLSv1.2 for compatibility with older clients
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
 
-$DownloadURL = 'https://raw.githubusercontent.com/8mpty/Windows_Post_Install_BATCH/main/Test-Install.cmd'
+$DownloadURL = 'https://raw.githubusercontent.com/8mpty/Windows_Post_Install_BATCH/main/Install.cmd'
 
-$FilePath = "$env:TEMP\Test-Post_Install.cmd"
+$FilePath = "$env:TEMP\Post_Install.cmd"
 
 try {
     Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing -OutFile $FilePath
