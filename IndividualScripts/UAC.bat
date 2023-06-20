7161df0db97399197d7222a605582140deb89f78
8mpty <mohdkhairin62@gmail.com>
2023-06-21 04:55:24 +0800

    Add UAC but gitignore not working???

---
+++ IndividualScripts/UAC.bat
@@ -0,0 +1,101 @@
+@echo off
+rem CenterSelf
+mode 67, 30
+
+CLS
+ ECHO.
+ ECHO =============================
+ ECHO Running Admin shell
+ ECHO =============================
+
+:init
+ setlocal DisableDelayedExpansion
+ set cmdInvoke=1
+ set winSysFolder=System32
+ set "batchPath=%~dpnx0"
+ rem this works also from cmd shell, other than %~0
+ for %%k in (%0) do set batchName=%%~nk
+ set "vbsGetPrivileges=%temp%\OEgetPriv_%batchName%.vbs"
+ setlocal EnableDelayedExpansion
+
+:checkPrivileges
+  NET FILE 1>NUL 2>NUL
+  if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
+
+:getPrivileges
+  if '%1'=='ELEV' (echo ELEV & shift /1 & goto gotPrivileges)
+  ECHO.
+  ECHO **************************************
+  ECHO Invoking UAC for Privilege Escalation
+  ECHO **************************************
+
+  ECHO Set UAC = CreateObject^("Shell.Application"^) > "%vbsGetPrivileges%"
+  ECHO args = "ELEV " >> "%vbsGetPrivileges%"
+  ECHO For Each strArg in WScript.Arguments >> "%vbsGetPrivileges%"
+  ECHO args = args ^& strArg ^& " "  >> "%vbsGetPrivileges%"
+  ECHO Next >> "%vbsGetPrivileges%"
+  
+  if '%cmdInvoke%'=='1' goto InvokeCmd 
+
+  ECHO UAC.ShellExecute "!batchPath!", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+  goto ExecElevation
+
+:InvokeCmd
+  ECHO args = "/c """ + "!batchPath!" + """ " + args >> "%vbsGetPrivileges%"
+  ECHO UAC.ShellExecute "%SystemRoot%\%winSysFolder%\cmd.exe", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+
+:ExecElevation
+ "%SystemRoot%\%winSysFolder%\WScript.exe" "%vbsGetPrivileges%" %*
+ exit /B
+
+:gotPrivileges
+ setlocal & cd /d %~dp0
+ if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul  &  shift /1)
+
+ ::::::::::::::::::::::::::::
+ ::START
+ ::::::::::::::::::::::::::::
+ REM Run shell as admin (example) - put here code as you like
+
+:start
+cls
+
+echo ----------------------------------------------------------------
+echo                         Enable Admin UAC                       
+echo ----------------------------------------------------------------
+
+echo.
+echo 1. Enable Admin Verifications in UAC
+echo 2. Disable Admin Verifications in UAC (default)
+echo 0. Exit
+echo.
+
+echo Enter choice in your keyboard [1,2,0]: 
+choice /C:120 /N
+set _erl_uac=%errorlevel%
+
+if %_erl_uac%==0 goto end
+if %_erl_uac%==2 goto dis_uac
+if %_erl_uac%==1 goto en_uac
+
+goto end
+
+:en_uac
+cls
+echo Enabling
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /f /v "ConsentPromptBehaviorAdmin" /t REG_DWORD /d 3
+ping 127.0.0.1 -n 2 -w 1000 > NUL
+echo DONE!!
+goto start
+
+:dis_uac
+cls
+echo Disabling
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /f /v "ConsentPromptBehaviorAdmin" /t REG_DWORD /d 4
+ping 127.0.0.1 -n 2 -w 1000 > NUL
+echo DONE!!
+goto start
+
+
+:end
+exit /b
