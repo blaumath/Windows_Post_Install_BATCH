7744bde0849c984dc25d8e69ec048cad603d299e
8mpty <mohdkhairin62@gmail.com>
2023-07-08 20:39:43 +0800

    separate scripts into extra pages as they are getting out of hand :anguished:

--- Install.bat
+++ Install.bat
@@ -57,47 +57,70 @@ CLS
  ::::::::::::::::::::::::::::
  REM Run shell as admin (example) - put here code as you like
 
-:start
+IF NOT EXIST %temp%\POST_TEMP\ (cd ./IndividualScripts/) else ( cd /D %temp%/POST_TEMP/)
+set ps=powershell.exe -NoProfile -ExecutionPolicy Unrestricted -Command "
 title AFTER_WINDOWS_INSTALL
 mode 67, 30
 
-IF NOT EXIST %temp%\POST_TEMP\ (cd ./IndividualScripts/) else ( cd /D %temp%/POST_TEMP/)
+:start
 cls
-
-set ps=powershell.exe -NoProfile -ExecutionPolicy Unrestricted -Command "
 
 echo ----------------------------------------------------------------
 echo                         Choose Options                          
 echo ----------------------------------------------------------------
 echo.
-echo 1. Activate Windows 10
-echo 2. ChrisTitusTech's Programs Install Manager
-echo 3. Standalone Winget Install
-echo 4. Enable/Disable Windows AutoLogin
-echo 5. Extract all Drivers
-echo 6. Download Debloater Scripts/Programs
-echo 7. Remove/Restore Folders from "This PC"
-echo 8. Enable/Disable UAC Verification
-echo 9. Backup Firefox Settings
-echo 0. Exit
+echo  [1]. Activate Windows 10
+echo  [2]. ChrisTitusTech's Programs Install Manager
+echo  [3]. Enable/Disable Windows AutoLogin
+echo  [4]. Download Debloater Scripts/Programs
+echo  [5]. Extras
+echo  [0]. Exit
 echo.
 
 
-echo Enter choice on your keyboard [1,2,3,4,5,6,7,8,90]: 
-choice /C:1234567890 /N
+echo Enter choice on your keyboard [1,2,3,4,5,0]: 
+choice /C:123450 /N
 set _erl=%errorlevel%
 
 
 if %_erl%==0 goto end
-if %_erl%==9 goto ff
-if %_erl%==8 goto uac_verification
-if %_erl%==7 goto remove_restore_folders
-if %_erl%==6 goto download_debloaters
-if %_erl%==5 goto ext_driver
-if %_erl%==4 goto wal
-if %_erl%==3 goto wg
+if %_erl%==5 goto misc
+if %_erl%==4 goto download_debloaters
+if %_erl%==3 goto wal
 if %_erl%==2 goto ctt
 if %_erl%==1 goto mass
+
+goto end
+
+:misc
+cls
+
+cls
+echo ----------------------------------------------------------------
+echo                      Choose Options [Extras]                    
+echo ----------------------------------------------------------------
+echo.
+echo [1]. Standalone Winget Install
+echo [2]. Extract all Drivers
+echo [3]. Remove/Restore Folders from "This PC"
+echo [4]. Enable/Disable UAC Verification
+echo [5]. Backup Firefox Settings
+echo [9]. Go Back
+echo [0]. Exit
+echo.
+
+echo Enter choice on your keyboard [1,2,3,4,5,9,0]: 
+choice /C:1234590 /N
+set _erl=%errorlevel%
+
+
+if %_erl%==0 goto end
+if %_erl%==6 goto start
+if %_erl%==5 goto ff
+if %_erl%==4 goto uac_verification
+if %_erl%==3 goto remove_restore_folders
+if %_erl%==2 goto ext_driver
+if %_erl%==1 goto wg
 
 goto end
 
