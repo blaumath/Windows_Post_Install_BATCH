0a29dba6354859c4bb7248e82dbe8366231ba7b5
8mpty <mohdkhairin62@gmail.com>
2023-07-24 01:26:58 +0800

    added extra options for UAC

--- IndividualScripts/UAC.bat
+++ IndividualScripts/UAC.bat
@@ -66,16 +66,18 @@ echo ----------------------------------------------------------------
 
 echo.
 echo 1. Enable Admin Verifications in UAC
-echo 2. Disable Admin Verifications in UAC (default)
+echo 2. Disable + Enhanced (More Secure)
+echo 2. Disable (Less Secure) (Windows Default)*
 echo 0. Exit
 echo.
 
-echo Enter choice in your keyboard [1,2,0]: 
-choice /C:120 /N
+echo Enter choice in your keyboard [1,2,3,0]: 
+choice /C:1230 /N
 set _erl_uac=%errorlevel%
 
 if %_erl_uac%==0 goto end
-if %_erl_uac%==2 goto dis_uac
+if %_erl_uac%==3 goto dis_uac_d
+if %_erl_uac%==2 goto dis_uac_e
 if %_erl_uac%==1 goto en_uac
 
 goto end
@@ -88,14 +90,21 @@ ping 127.0.0.1 -n 2 -w 1000 > NUL
 echo DONE!!
 goto start
 
-:dis_uac
+:dis_uac_e
+cls
+echo Disabling (Setting with Improvements)
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /f /v "ConsentPromptBehaviorAdmin" /t REG_DWORD /d 2
+ping 127.0.0.1 -n 2 -w 1000 > NUL
+echo DONE!!
+goto start
+
+:dis_uac_d
 cls
 echo Disabling
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /f /v "ConsentPromptBehaviorAdmin" /t REG_DWORD /d 5
 ping 127.0.0.1 -n 2 -w 1000 > NUL
 echo DONE!!
 goto start
-
 
 :end
 exit /b
