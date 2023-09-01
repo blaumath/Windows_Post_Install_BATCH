f3b698cc182b4f050d391783005ac55e5070a63f
8mpty <mohdkhairin62@gmail.com>
2023-09-01 17:56:31 +0800

    user is able to add 2 arguments now. Will update readme in  a bit😁

--- psfile/empty.ps1
+++ psfile/empty.ps1
@@ -37,11 +37,8 @@ try {
 if (Test-Path $MainPath) {
     if ($args.Count -ge 1) {
         $op = $args[0]
-        
-        # Join the remaining arguments into a single string
-        $argList = $args[1..($args.Length - 1)] -join ' '
-
-        Start-Process -FilePath "$MainPath\Install.bat" -ArgumentList "$op $argList" -Wait
+        $op2 = $args[1..($args.Length - 1)] -join ' '
+        Start-Process -FilePath "$MainPath\Install.bat" -ArgumentList "$op $op2" -Wait
     } else {
         Start-Process -FilePath "$MainPath\Install.bat" -Wait    
     }
