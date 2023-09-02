bffd6b63f7975bce7d3cb4ccbdce32c87a91114e
8mpty <mohdkhairin62@gmail.com>
2023-09-03 02:56:25 +0800

    Update empty.ps1

--- psfile/empty.ps1
+++ psfile/empty.ps1
@@ -41,23 +41,12 @@ try {
 
 
 if (Test-Path $MainPath) {
-    
-    <#if ($args.Count -ge 1 -and $args[0] -eq "/s") {
-        $op = $args[0]
-        $op2 = $args[1..($args.Length - 1)] -join ' '
-        Write-Host "WITH" $op $op2
-        Start-Process -FilePath "$MainPath\Install.bat" -ArgumentList "$op $op2" -NoNewWindow -Wait
-    }#>
-    
     if ($args.Count -ge 1) {
         $op = $args[0]
         $op2 = $args[1..($args.Length - 1)] -join ' '
-        Write-Host "NOO" $op $op2
         Start-Process -FilePath "$MainPath\Install.bat" -ArgumentList "$op $op2" -Wait
     } else {
-        Write-Host "NORMALL"
         Start-Process -FilePath "$MainPath\Install.bat" -Wait    
     }
-
     Remove-Item -Path $MainPath -Recurse -Force
 }
